55516d4408a2dd962bec4a4325f5780f70541c94
Graham Abbott <graham.abbott@gmail.com>
2014-09-21 11:08:36 -0700

    adding ignore as well as a script to make prepping for test

---
+++ prepare.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+
+import os
+import json
+
+try:
+    os.mkdir('build')
+except:
+    pass
+
+import urllib
+print 'downloading latest genie library...'
+urllib.urlretrieve(
+    url='https://raw.githubusercontent.com/graham/genie/master/build/genie.min.js', 
+    filename='build/genie.min.js')
+
+print 'downloading latest jquery...'
+urllib.urlretrieve(
+    url='http://code.jquery.com/jquery-2.1.1.min.js',
+    filename='build/jquery.js')
+
+def get_components(path):
+    components = []
+
+    for i in os.listdir(path):
+        rpath = path + '/' + i
+        print rpath
+        if rpath.startswith('.'):
+            pass
+        elif rpath.endswith('~'):
+            pass
+        elif os.path.isdir(rpath):
+            components.extend(get_components(rpath))
+        elif rpath.endswith('.html') or rpath.endswith('.txt'):
+            components.append(rpath)
+
+    return components
+
+print 'finding components...'    
+comps = get_components('components')
+print ''
+
+
+print 'saving...'
+f = open('list.json', 'w')
+f.write(json.dumps({'list':comps}, indent=True))
+f.close()
+print '#done'
